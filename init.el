58f7bf82edf9aa672e650b407fa1b311d4755228
David Siefert <siefert.david@gmail.com>
2017-12-19 13:16:16 -0600

    add exec-path-from-shell

--- init.el
+++ init.el
@@ -4,7 +4,7 @@
 
 (package-initialize)
 
-(setq package-list '(go-mode))
+(setq package-list '(go-mode exec-path-from-shell))
 
 (unless package-archive-contents
   (package-refresh-contents))
@@ -12,12 +12,13 @@
 (dolist (package package-list)
   (unless (package-installed-p package)
     (package-install package)))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (go-mode))))
+ '(package-selected-packages (quote (exec-path-from-shell go-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
